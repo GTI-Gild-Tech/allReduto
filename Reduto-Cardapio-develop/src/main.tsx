a08da6d4ede5700acec6479bc7c36090a371b1b8
Ilca Almeida <ilcalmeida92@gmail.com>
2026-09-04 14:55:00 -0300

    fix: Remove unnecessary indentation and ensure consistent formatting in main.tsx

--- Reduto-Cardapio-develop/src/main.tsx
+++ Reduto-Cardapio-develop/src/main.tsx
@@ -1,8 +1,6 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
-
-  console.log("Iniciou o App React"), // <- para debug
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+console.log("Iniciou o App React"); // <- para debug
+createRoot(document.getElementById("root")!).render(<App />);
